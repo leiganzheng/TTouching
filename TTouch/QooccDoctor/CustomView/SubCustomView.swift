cc0dcea17c9148797b44d3239ed54d53c9a46b0e
leiganzheng <qoocc03@qoocc03deiMac.local>
2016-05-31 16:09:34 +0800

    xiug bug

---
+++ TTouch/QooccDoctor/CustomView/SubCustomView.swift
@@ -0,0 +1,27 @@
+//
+//  SubCustomView.swift
+//  QooccDoctor
+//
+//  Created by leiganzheng on 16/5/31.
+//  Copyright © 2016年 juxi. All rights reserved.
+//
+
+import UIKit
+
+class SubCustomView: UIView {
+
+    var count:Int?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)!
+    }
+    
+    func updateLayerFrames() {
+        
+    }
+
+}
---
+++ TTouch/QooccDoctor/UserList/ShakeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ShakeViewController.swift
+//  QooccDoctor
+//
+//  Created by leiganzheng on 16/5/31.
+//  Copyright © 2016年 juxi. All rights reserved.
+//
+
+import UIKit
+
+class ShakeViewController: UIViewController {
+
+    @IBOutlet weak var selectButton: UISwitch!
+    @IBOutlet weak var zoneView: UIView!
+    @IBOutlet weak var zoneBtn: UIButton!
+    @IBOutlet weak var screenView: UIView!
+    @IBOutlet weak var screenBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
